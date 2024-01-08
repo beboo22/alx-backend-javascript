ff8646c70765d4bc7e1b0982145fa2bb0100c6a2
beboo22 <mt557829@gmail.com>
2024-01-08 22:41:31 +0200

    task

--- 0x00-ES6_basic/2-arrow.js
+++ 0x00-ES6_basic/2-arrow.js
@@ -1,8 +1,9 @@
-export default function getNeighborhoodsList (){
-    this.x = [ 'SOMA', 'Union Square' ];
+export default function getNeighborhoodsList() {
+    this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
+  
     const self = this;
-    this.addNeighborhood = function add(n){
-        self.x.push(n);
-        return self.x;
-    }
-}
+    this.addNeighborhood = (newNeighborhood) => {
+      self.sanFranciscoNeighborhoods.push(newNeighborhood);
+      return self.sanFranciscoNeighborhoods;
+    };
+  }
