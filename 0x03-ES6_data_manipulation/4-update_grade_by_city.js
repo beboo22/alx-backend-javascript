b0e486a1b3aa6f2577245f6ef963d02af775d8cc
beboo22 <mt557829@gmail.com>
2024-01-09 22:44:39 +0200

    task

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -6,9 +6,8 @@
 export default function updateStudentGradeByCity(std, city, newGrades) {
   const defualtgard = { grade: 'N/A' };
   if (std instanceof Array) {
-    return std.filter(
-      (stud) => stud.location === city,
-    )
+    return std
+      .filter((stud) => stud.location === city)
       .map((stud) => ({
         id: stud.id,
         firstName: stud.firstName,
