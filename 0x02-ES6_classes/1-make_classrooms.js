a4ccf050bfd211ade84191c443fe598ef61c85a4
beboo22 <mt557829@gmail.com>
2024-01-21 15:57:09 +0200

    task

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,4 +1,5 @@
-import ClassRoom from './0-classroom.js';
+// eslint-disable-next-line import/extensions
+import ClassRoom from './0-classroom';
 
 export default function initializeRooms() {
   const obj = [new ClassRoom(19), new ClassRoom(20), new ClassRoom(34)];
