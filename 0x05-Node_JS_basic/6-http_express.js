3ca50ff2f0c14970898bfd117b0cb1b6a1a097f7
beboo22 <mt557829@gmail.com>
2024-02-29 00:08:00 +0200

    task

--- 0x05-Node_JS_basic/6-http_express.js
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -1,14 +1,9 @@
+#!/usr/bin/node
 const express = require('express');
 
 const app = express();
-const port = 1245;
-
 app.get('/', (req, res) => {
-  res.send('Hello Holberton School!\n');
+  res.send('Hello Holberton School!');
 });
-
-app.listen(port, () => {
-  console.log('...');
-});
-
+app.listen(1245);
 module.exports = app;
