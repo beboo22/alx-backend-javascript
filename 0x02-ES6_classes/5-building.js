0f4987fe4c69ec53946e4fbe7710999c34b486cd
beboo22 <mt557829@gmail.com>
2024-02-29 00:05:04 +0200

    task

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,13 @@
+export default class Building {
+  constructor(sqft) {
+    this.sqft = sqft;
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  set sqft(value) {
+    this._sqft = value;
+  }
+}
