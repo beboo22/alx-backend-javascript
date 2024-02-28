ed52071bc5bd1712d1df7d562bcb32bacc58d221
beboo22 <mt557829@gmail.com>
2024-02-28 23:11:54 +0200

    task

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,14 +1,18 @@
 #!/usr/bin/node
-const fs = require('fs');
+const fs = require('fs').promises;
 
-function countStudents (path) {
-  fs.access(path, fs.constants.F_OK, (err) => {
-    if (err) throw new Error('Cannot load the database');
-  });
-  const db = fs.readFileSync(path, 'utf-8');
+async function countStudents(path) {
+  try {
+    await fs.access(path, fs.constants.F_OK);
+  } catch (err) {
+    throw new Error('Cannot load the database');
+  }
+
+  const db = await fs.readFile(path, 'utf-8');
   const result = db.split('\n').map((line) => line.split(','));
   const data = {};
   let num = 0;
+
   result.forEach((element) => {
     if (element.length === 4) {
       const field = element[3];
@@ -21,11 +25,14 @@ function countStudents (path) {
       num += 1;
     }
   });
+
   delete data.field;
   num -= 1;
+
   console.log(`Number of students: ${num}`);
   Object.entries(data).forEach(([key, val]) => {
     console.log(`Number of students in ${key}: ${val.length}. List: ${val.join(', ')}`);
   });
 }
+
 module.exports = countStudents;
