f19aaf993c6ef29407b2c8e88871baa257bf5315
beboo22 <mt557829@gmail.com>
2024-03-01 01:13:24 +0200

    task

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -1,4 +1,4 @@
-const  expect  = require('chai');
+const  {expect}  = require('chai');
 const calculateNumber = require('./2-calcul_chai');
 
 describe('test function calculateNumber in 1-calcul', () => {
