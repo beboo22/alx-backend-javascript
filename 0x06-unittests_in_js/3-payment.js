7b467392dd2b1d243004b1f3d7d5e586dd461cd7
beboo22 <mt557829@gmail.com>
2024-02-29 19:41:42 +0200

    task

--- 0x06-unittests_in_js/3-payment.js
+++ 0x06-unittests_in_js/3-payment.js
@@ -1,14 +1,6 @@
-const calculateNumber = (type, a, b) => {
-  if (type === 'SUM') {
-    return Math.round(a) + Math.round(b);
-  }
-  if (type === 'SUBTRACT') {
-    return Math.round(a) - Math.round(b);
-  }
-  if (type === 'DIVIDE') {
-    return Math.round(b) === 0 ? 'Error' : Math.round(a) / Math.round(b);
-  }
-  return 0;
+const sendPaymentRequestToApi = (totalAmount, totalShipping) => {
+  const result = Utils.calculateNumber(totalAmount, totalShipping);
+  console.log(`The total is: ${result}`);
 };
 
 module.exports = calculateNumber;
--- 0x06-unittests_in_js/README.md
+++ 0x06-unittests_in_js/README.md
@@ -1,2 +1,2 @@
 0x06-unittests_in_js
-npm test 1-calcul.js
+npm test 2-calcul.test.js
