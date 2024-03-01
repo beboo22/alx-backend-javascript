f653d201fcdc676ef9a266aee19d01528103bcc3
beboo22 <mt557829@gmail.com>
2024-03-01 16:27:31 +0200

    task

---
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -0,0 +1,18 @@
+const { expect } = require('chai');
+const request = require('request');
+
+describe('test API', () => {
+  it('test status code of API', (done) => {
+    request.get('http://localhost:7865', (err, res, body) => {
+      expect(res.statusCode).to.equal(200);
+      done();
+    });
+  });
+
+  it('test result code of API', (done) => {
+    request.get('http://localhost:7865', (err, res, body) => {
+      expect(body).to.equal('Welcome to the payment system');
+      done();
+    });
+  });
+});
