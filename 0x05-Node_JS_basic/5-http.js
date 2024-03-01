eac350817f9abbe97b5248f20dacf9bbdf1fe7d6
beboo22 <mt557829@gmail.com>
2024-03-01 14:47:38 +0200

    task

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,72 +1,61 @@
+#!/usr/bin/node
 const http = require('http');
-const fs = require('fs').promises;
+const fs = require('fs');
 
-const countStudentsPromise = (path, res) => {
-  return new Promise(async (resolve, reject) => {
-    try {
-      await countStudents(path, res);
+function countStudents(path, res) {
+  const promise = new Promise((resolve, reject) => {
+    fs.readFile(path, 'utf8', (err, d) => {
+      if (err) {
+        reject(Error('Cannot load the database'));
+        return;
+      }
+      const db = d.split('\n');
+      const data = {};
+      let num = 0;
+      db.forEach((row) => {
+        const cols = row.split(',');
+        if (cols.length === 4) {
+          const dep = cols[3];
+          const name = cols[0];
+          if (data[dep] === undefined) {
+            data[dep] = [name];
+          } else {
+            data[dep].push(name);
+          }
+          num += 1;
+        }
+      });
+      delete data.field;
+      num -= 1;
+      res.write(`Number of students: ${num}\n`);
+      let len = Object.keys(data).length;
+      Object.entries(data).forEach(([key, val]) => {
+        let cont = `Number of students in ${key}: ${val.length}. List: ${val.join(', ')}`;
+        if (len !== 1) {
+          cont += '\n';
+        }
+        len -= 1;
+        res.write(cont);
+      });
       resolve();
-    } catch (error) {
-      reject(error);
-    }
+    });
   });
-};
-
-async function countStudents(path, res) {
-  let db;
-  try {
-    db = await fs.readFile(path, 'utf-8');
-    db = db.split('\n').map((line) => line.split(','));
-  } catch (err) {
-    throw new Error('Cannot load the database');
-  }
-  const data = {};
-  let num = 0;
-
-  db.forEach((element) => {
-    if (element.length === 4) {
-      const field = element[3];
-      const name = element[0];
-      if (data[field] === undefined) {
-        data[field] = [name];
-      } else {
-        data[field].push(name);
-      }
-      num += 1;
-    }
-  });
-
-  delete data.field;
-  num -= 1;
-
-  res.write(`Number of students: ${num}\n`);
-  let len = Object.keys(data).length;
-  Object.entries(data).forEach(([key, val]) => {
-    let cont = `Number of students in ${key}: ${val.length}. List: ${val.join(', ')}`;
-      if (len !== 1) {
-        cont += '\n';
-      }
-      len -= 1;
-      res.write(cont);
-  });
-
-  res.end();
+  return promise;
 }
 
-const app = http.createServer(async (req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
+const app = http.createServer((req, res) => {
   if (req.url === '/') {
-    res.end('Hello Holberton School!');
+    res.write('Hello Holberton School!');
+    res.end();
   } else if (req.url === '/students') {
     res.write('This is the list of our students\n');
-    try {
-      await countStudentsPromise(process.argv[2], res);
-    } catch (e) {
-      res.end(`Error: ${e.message}`);
-    }
+    countStudents(process.argv[2], res).then(() => {
+      res.end();
+    }).catch((err) => {
+      res.end(err.message);
+    });
   }
 });
 
 app.listen(1245);
-
 module.exports = app;
